e856e8c0b95e37476f3704f1108f03cbe526c4b3
Jonathon Pennington <jpenni3@outlook.com>
2019-12-14 21:30:10 -0600

    initial commit

---
+++ wireframejs.js
@@ -0,0 +1,61 @@
+//Quiz App Questions Data
+const STORE = [
+    {
+      question: "Despite the movie, TOP DOG, being a loveable story about a KungFu Cop and his crime fighting dog, what led to it being a failure by critics and fans alike?",
+      answers: [
+        "The dog, Reno, was replaced by an animatronic puppet halfway through the movie due to scheduling conflicts",
+        "Chuck Norris, was reportedly unhappy with the films direction, causing obvious outtakes being left in for time",
+        "Sony Pictures cancelled the movie a week into production due to budgeting concerns, delaying the final release to theater by 8 months",
+        "Due to a distributing mistake, the movie was released just 9 days after the Oklahoma City Bombing"
+      ],
+      correctAnswer: "Due to a distributing mistake, the movie was released just 9 days after the Oklahoma City Bombing"
+        
+    },
+    {
+      question:
+        "Everyone Loves the Iconic Extraterrestrial E.T.  But what 1988 theater release about a boy and his alien was not given an iconic send-off?",
+      answers: [
+        "They Live",
+        "Mac and Me",
+        "Critters",
+        "My Little Green Friend"
+      ],
+      correctAnswer:
+        "Mac and Me"
+    },
+    {
+      question:
+        "D.J. Pooh co-wrote the movie <i>Friday</i>, but he is also is well known for directing <em><strong>THIS</strong></em>, one of the lowest-rated comedies on Rotten Tomatoes:",
+      answers: [
+        "3 Strikes",
+        "Mallrats",
+        "The Pacifier",
+        "Mutant Vampire Zombies from the Hood!"
+      ],
+      correctAnswer: "3 strikes"
+    },
+    {
+      question: `"You're Tearing Me Apart Lisa!" is just one of the iconic quotes from this seriously spoofed cult classic by Tommy Wiseau`,
+      answers: [
+        "Cold Moon",
+        "Best F(r)iends",
+        "The Room",
+        "Big Shark"
+      ],
+      correctAnswer: "The Room"
+    },
+    {
+      question:
+        "Jaws is well known for its actors, iconic lines, and giant sharks, but no one ever talks about the many sequels it spawned. Which is a real Jaws Sequel?",
+      answers: [
+        "Jaws: Stay out of the Water",
+        "Jaws: The Last Wake",
+        "Jaws: Amity Island",
+        "Jaws: The Revenge"
+      ],
+      correctAnswer:
+        "Jaws: The Revenge"
+    },
+  ];
+  
+ 
